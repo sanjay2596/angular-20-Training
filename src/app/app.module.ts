33c4d680dbbe6a64299bf14fdb5833f51f4f8b89
sanjay2596 <sanjay@ideas2it.com>
2020-04-23 17:43:28 +0530

    creation of new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewcomponentComponent } from './newcomponent/newcomponent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    NewcomponentComponent
+    ],
   imports: [
     BrowserModule,
     AppRoutingModule
