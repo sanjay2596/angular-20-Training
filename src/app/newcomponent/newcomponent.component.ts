ece4873ae3042f6d42c8489a0a8499456d53c4dd
sanjay2596 <sanjay@ideas2it.com>
2020-04-24 17:43:08 +0530

    class bindingO

--- src/app/newcomponent/newcomponent.component.ts
+++ src/app/newcomponent/newcomponent.component.ts
@@ -3,10 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-newcomponent',
   templateUrl: './newcomponent.component.html',
-  styleUrls: ['./newcomponent.component.css']
+  styles: [`
+  .text-success{
+    color:green;
+  }
+  .text-danger{
+    color:red;
+  }
+  .text-special{
+    font-style:italic;
+  }
+  `]
 })
 export class NewcomponentComponent implements OnInit {
   public isDisabled = true;
+
+  public hasError = false;
+  public isSpecial = true;
+
+  public messageClasses = {
+    "text-success":this.hasError,
+    "text-danger":!this.hasError,
+    "text-special":this.isSpecial
+  }
   constructor() { }
 
   ngOnInit() {
