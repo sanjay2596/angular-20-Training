ee73cdd4ddae410c415abc404932de77313c3e1f
sanjay2596 <sanjay@ideas2it.com>
2020-04-24 19:40:45 +0530

    display directive for ngIf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewcomponentComponent } from './newcomponent/newcomponent.component';
+import { DisplayDirectiveComponent } from './display-directive/display-directive.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NewcomponentComponent
+    NewcomponentComponent,
+    DisplayDirectiveComponent
     ],
   imports: [
     BrowserModule,
